7db7118eef2f470952af397c649cd7322a2160ff
Dimitris Drymousis <jimdrymos27@gmail.com>
2026-04-24 13:23:26 +0300

    fixed index style database and gitignore

--- thesis/database.py
+++ thesis/database.py
@@ -1,4 +1,8 @@
 import sqlite3
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "database.db")
 
 def get_connection():
     conn = sqlite3.connect("database.db")
