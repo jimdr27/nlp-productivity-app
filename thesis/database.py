19fdc3d5ba35fa5725aa115be84700be7037577a
Dimitris Drymousis <jimdrymos27@gmail.com>
2026-04-24 14:24:09 +0300

    fixed database py

--- thesis/database.py
+++ thesis/database.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "database.db")
 
 def get_connection():
-    conn = sqlite3.connect("DB_PATH")
+    conn = sqlite3.connect(DB_PATH)
     
     conn.row_factory = sqlite3.Row 
     return conn
